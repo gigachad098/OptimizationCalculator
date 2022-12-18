94fc7bb7bc3fc4d7fc4dcd9fce4addc29c19482f
gigachad098 <107569878+gigachad098@users.noreply.github.com>
2022-12-18 14:28:08 -0500

    Added product rule but doesn't work completely yet

--- src/Derive.java
+++ src/Derive.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-
 public class Derive {
    public static String[] breakingFunctionUp(final String input) {
       final String[] temp = input.split("\\^", 2);
-      final String[] test = new String[3];
+      final String[] test = new String[4];
       for (int i = 0; i < input.length(); i++) {
          if ((Character.isDigit(temp[0].charAt(i))) && (i == 0)) {
             test[0] = String.valueOf(temp[0].charAt(i));
@@ -16,9 +14,10 @@ public class Derive {
       }
       for (int j = 0; j < temp[1].length(); j++) {
          if (j == 0) {
-            test[2] = String.valueOf(temp[1].charAt(j));
+            test[2] = "^";
+            test[3] = String.valueOf(temp[1].charAt(j));
          } else {
-            test[2] += String.valueOf(temp[1].charAt(j));
+            test[3] += String.valueOf(temp[1].charAt(j));
          }
       }
       return test;
@@ -26,16 +25,26 @@ public class Derive {
 
    public static String powerRule(final String input) {
       final String[] function = breakingFunctionUp(input);
-      return String.valueOf(Integer.parseInt(function[0]) * Integer.parseInt(function[2])) + function[1]
-            + (Integer.parseInt(function[2]) - 1);
+      return (Integer.parseInt(function[0]) * Integer.parseInt(function[3])) + function[1] + function[2]
+            + (Integer.parseInt(function[3]) - 1);
    }
    public static String polynomialMultiplication (final String input1, final String input2) {
       final String[] function1 = breakingFunctionUp(input1);
       final String[] function2 = breakingFunctionUp(input2);
-      return String.valueOf(Integer.parseInt(function1[0]) + Integer.parseInt(function2[0])) + function1[1] + String.valueOf(Integer.parseInt(function1[2] + function2[2]));
+      return (Integer.parseInt(function1[0]) * Integer.parseInt(function2[0])) + function1[1] + function1[2] + (Integer.parseInt(function1[3]) + Integer.parseInt(function2[3]));
    }
    public static String productRule (final String functionOne, final String functionTwo) {
-      return polynomialMultiplication(powerRule(functionOne), functionTwo) + polynomialMultiplication(powerRule(functionTwo), functionOne);
+      String temp = polynomialMultiplication(powerRule(functionOne), functionTwo) + " " + polynomialMultiplication(powerRule(functionTwo), functionOne);
+      return polynomialSimplification(temp);
+   }
+   public static String polynomialSimplification (final String function) {
+      String[] temp = function.split("\\s", 2);
+      final String[] temp0Broke = breakingFunctionUp(temp[0]);
+      final String[] temp1Broke = breakingFunctionUp(temp[1]);
+      if (Integer.parseInt(temp0Broke[3]) == Integer.parseInt(temp1Broke[3])) {
+         return (Integer.parseInt(temp0Broke[0]) + Integer.parseInt(temp1Broke[0])) + temp0Broke[1] + temp0Broke[2] + temp0Broke[3];
+      }
+      else return temp0Broke[0] + temp0Broke[1] + temp0Broke[2] + temp0Broke[3] + " + " + temp1Broke[0] + temp1Broke[1] + temp1Broke[2] + temp1Broke[3];
    }
 }
 
